7b5d8baaf851810ebbed8f9737ff6bf871d00334
Arthur Delannoy <ah.delannoy@gmail.com>
2025-11-27 15:22:27 +0100

    removed bash preexec from docker

--- hosts/devcontainer/default.nix
+++ hosts/devcontainer/default.nix
@@ -1,13 +1,11 @@
 { pkgs, dotfilesInput, ... }:
 
 let
-  # 1. Dotfiles Storage (Read-Only)
   dotfilesLayer = pkgs.runCommand "dotfiles-layer" { } ''
     mkdir -p $out/opt
     cp -r ${dotfilesInput} $out/opt/dotfiles
   '';
 
-  # 2. Entrypoint Script
   entrypointScript = pkgs.writeScriptBin "entrypoint.sh" ''
     #!${pkgs.bash}/bin/bash
     set -e
@@ -32,8 +30,6 @@ let
     exec "$@"
   '';
 
-  # 3. System Config (Passwd/Group)
-  # We ONLY do /etc files here. We do NOT create /home here.
   devSetup = pkgs.runCommand "dev-setup" { } ''
     mkdir -p $out/etc
     echo "root:x:0:0:root:/root:/bin/bash" > $out/etc/passwd
@@ -41,13 +37,6 @@ let
     echo "root:x:0:" > $out/etc/group
     echo "arthur:x:1000:" >> $out/etc/group
     echo "hosts: files dns" > $out/etc/nsswitch.conf
-
-    # --- bash-preexec Symlink (ATUIN) ---
-    # We create the standard directory
-    mkdir -p $out/usr/share/bash-preexec
-    
-    # We link the file from the Nix store to the standard path
-    ln -s ${pkgs.bash-preexec}/share/bash-preexec/bash-preexec.sh $out/usr/share/bash-preexec/bash-preexec.sh
   '';
 
 in
@@ -57,8 +46,8 @@ pkgs.dockerTools.buildLayeredImage {
 
   contents = with pkgs; [
     devSetup
-    dotfilesLayer      # <--- The baked-in files
-    entrypointScript   # <--- The script to install them
+    dotfilesLayer
+    entrypointScript
     
     # --- Base Utils ---
     bashInteractive
@@ -77,15 +66,14 @@ pkgs.dockerTools.buildLayeredImage {
     nil
 
     # --- 4. ESSENTIAL LINUX TOOLS (ADDED) ---
-    gnugrep   # Fixes 'grep: command not found'
-    gnused    # Essential for scripts (sed)
-    findutils # Essential for scripts (find, xargs)
-    gawk      # awk
-    which     # Useful to check paths
+    gnugrep
+    gnused
+    findutils
+    gawk
+    which
     util-linux
     
     # --- Terminal Tools ---
-    bash-preexec
     atuin
     btop
     tree
@@ -110,17 +98,12 @@ pkgs.dockerTools.buildLayeredImage {
     nix
     uv
   ];
-  # 4. FIX PERMISSIONS HERE
-  # We use relative paths (./home) because this script runs in the build root.
+  
   fakeRootCommands = ''
     mkdir -p ./home/arthur
     mkdir -p ./tmp
-
-    # Set ownership to 1000 (arthur)
     chown -R 1000:1000 ./home/arthur
     chown -R 1000:1000 ./tmp
-    
-    # Set permissions (Owner can write)
     chmod 755 ./home/arthur
     chmod 1777 ./tmp
   '';
@@ -134,8 +117,8 @@ pkgs.dockerTools.buildLayeredImage {
     Env = [
       "USER=arthur"
       "HOME=/home/arthur"
+      # Explicitly set history file location for Bash
       "HISTFILE=/home/arthur/.bash_history"
-      "BASH_PREEXEC_PATH=${pkgs.bash-preexec}/share/bash-preexec/bash-preexec.sh"
       "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
       "GIT_SSL_CAINFO=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
       "PATH=/bin:/usr/bin:/usr/local/bin"
