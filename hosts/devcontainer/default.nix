10d50941287908a99751ab8b4122f73b20372ef5
Arthur Delannoy <ah.delannoy@gmail.com>
2025-11-27 12:04:02 +0100

    niw docker folder fix

--- hosts/devcontainer/default.nix
+++ hosts/devcontainer/default.nix
@@ -1,26 +1,30 @@
 { pkgs, ... }:
 
+let
+  # 1. We create a "package" that just contains the folder structure we need.
+  # Nix will merge this into the root of the Docker image.
+  dirSetup = pkgs.runCommand "dev-setup-dirs" { } ''
+    mkdir -p $out/home/arthur
+    mkdir -p $out/tmp
+    chmod 1777 $out/tmp
+  '';
+in
 pkgs.dockerTools.buildLayeredImage {
   name = "nix-devcontainer";
   tag = "latest";
-  
-  # Created specifically for non-root usage (UID 1000)
-  fakeRootCommands = ''
-    mkdir -p /home/arthur
-    chown 1000:1000 /home/arthur
-    mkdir -p /tmp
-    chmod 1777 /tmp
-  '';
 
+  # 2. We add 'dirSetup' to the contents
   contents = with pkgs; [
+    dirSetup # <--- This adds /home/arthur and /tmp
+    
     # --- Base Utils ---
     bashInteractive
     coreutils
     git
-    cacert  # Crucial for HTTPS (git clone, curl, etc)
+    cacert
     curl
     wget
-    iana-etc # Fixes "unknown protocol" errors
+    iana-etc
     
     # --- From your modules/terminal ---
     btop
@@ -35,41 +39,34 @@ pkgs.dockerTools.buildLayeredImage {
     fzf
     tldr
     
-    # --- Dev Tools (Mirroring your modules/dev) ---
-    # Add the specific compilers/tools you need here
+    # --- Dev Tools ---
     vim
     python3
-    uv
     nodejs
     gnumake
     gcc
     ripgrep
     fd
-    
-    # --- Nix itself (Optional) ---
-    # Useful if you want to run 'nix flake check' inside the container
-    # Note: Requires multi-user daemon setup usually, strictly basic here.
-    nix 
+    nix
   ];
 
+  # 3. Use fakeRootCommands ONLY for permissions (not creation)
+  # '|| true' ensures the build doesn't crash if fakeroot acts weirdly, 
+  # but since we mount a PVC at /home/arthur anyway, the PVC permissions will take over.
+  fakeRootCommands = ''
+    chown -R 1000:1000 /home/arthur || true
+  '';
+
   config = {
-    # Run as the standard non-root user (usually mapped to 1000 in K8s/Rancher)
     User = "1000";
-    
-    # This matches your PVC mount point logic
     WorkingDir = "/home/arthur";
-    
-    # Set up environment variables
     Env = [
       "USER=arthur"
       "HOME=/home/arthur"
       "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
       "GIT_SSL_CAINFO=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-      # Nix automatically handles PATH, but we ensure /bin is there
       "PATH=/bin:/usr/bin:/usr/local/bin"
     ];
-    
-    # Default command when container starts
     Cmd = [ "/bin/bash" ];
   };
 }
