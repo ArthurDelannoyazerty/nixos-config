fce76e460c270f1b2f43d24360e639e9af9abfe7
Arthur Delannoy <ah.delannoy@gmail.com>
2026-04-29 21:34:22 +0200

    updated authentik

--- modules/constants.nix
+++ modules/constants.nix
@@ -28,7 +28,7 @@ rec {
     authentik = {
       port = 9000;
       subdomain = "authentik";
-      version = "2025.12.4";
+      version = "2026.2.2";
       containerName = "authentik-server";
     };
     authentik-db = {
@@ -38,7 +38,7 @@ rec {
     };
     authentik-worker = {
       port = 9000;
-      version = "2025.12.4";
+      version = "2026.2.2";
       containerName = "authentik-worker";
     };
     bazarr = { 
