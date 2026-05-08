3d7caadaecd5f1cb9af1cb72cd5e816545de13a7
Arthur Delannoy <ah.delannoy@gmail.com>
2026-05-08 17:58:47 +0200

    updated scanopy logo in homepage

--- hosts/homelab/services/random/homepage.nix
+++ hosts/homelab/services/random/homepage.nix
@@ -216,7 +216,7 @@ let
                 url: ${internalHost}:${toString myConstants.services.uptime-kuma.port}
                 slug: default 
         - Scanopy:
-            icon: https://scanopy.net/scanopy-logo-64.webp
+            icon: https://scanopy.net/scanopy-logo-64.webp"
             href: https://${myConstants.services.scanopy.subdomain}.${myConstants.publicDomain}
             description: Network Inventory & Scanner
             server: my-docker
