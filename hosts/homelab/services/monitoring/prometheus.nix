5ec17c7c47569b5d7aaeea5a97fafe549d964f4d
Arthur Delannoy <ah.delannoy@gmail.com>
2026-07-17 22:46:55 +0200

    changed prometheus location to the disk

--- hosts/homelab/services/monitoring/prometheus.nix
+++ hosts/homelab/services/monitoring/prometheus.nix
@@ -5,25 +5,32 @@
     enable = true;
     port = myConstants.services.prometheus.port;
 
-    # Keep 30 days of historical data
+    # Keep 2 years of historical data
     retentionTime = "2y"; 
 
     scrapeConfigs =[
       {
         job_name = "netdata";
-        scrape_interval = "5s"; 
+        scrape_interval = "30s"; 
         metrics_path = "/api/v1/allmetrics";
         params = {
-          # Ask Netdata to format the output for Prometheus
           format =[ "prometheus" ]; 
         };
         static_configs =[
           {
-            # Dynamically pull the port from constants.nix
             targets =[ "127.0.0.1:${toString myConstants.services.netdata.port}" ];
           }
         ];
       }
     ];
   };
+
+  # Automatically manage the 4TB HDD storage directory and symlink it to the state path
+  systemd.tmpfiles.rules = [
+    # Creates the storage directory on the HDD with correct permissions if it doesn't exist
+    "d ${myConstants.paths.services4TB}/prometheus2 0751 prometheus prometheus - -"
+
+    # Creates a symlink pointing the SSD's data folder to the HDD location
+    "L+ /var/lib/${config.services.prometheus.stateDir}/data - - - - ${myConstants.paths.services4TB}/prometheus2"
+  ];
 }
