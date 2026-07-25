e5d406597d7d26d5e35f03d909a7cb6bd4216653
Arthur Delannoy <ah.delannoy@gmail.com>
2026-07-25 19:14:03 +0200

    fix whats up docker

--- hosts/homelab/services/monitoring/whats-up-docker.nix
+++ hosts/homelab/services/monitoring/whats-up-docker.nix
@@ -8,12 +8,13 @@
     ports = [ "172.17.0.1:${toString myConstants.services.whats-up-docker.port}:3000" ];
     
     environment = {
-      # Use the docker socket proxy instead of /var/run/docker.sock
-      WUD_WATCHER_LOCAL_HOST = myConstants.dockerSocketProxy;
+      WUD_WATCHER_LOCAL_HOST = "socket-proxy";
       WUD_WATCHER_LOCAL_PORT = "2375";
       
       # Also set standard DOCKER_HOST as fallback for standard Docker integrations
       DOCKER_HOST = "tcp://${myConstants.dockerSocketProxy}:2375";
+
+      WUD_WATCHER_LOCAL_WATCHDIGEST = "true";
     };
 
     volumes = [
